85948d47ba6c9b8a1b35a3b921b45e170466cd76
王永杰GithubPro <17633559293@163.com>
2018-08-22 21:57:07 +0800

    添加一键启动

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var animatedName = ["bounce", "flash", "pulse", "rubberBand", "shake", "headShake", "swing", "tada", "wobble", "jello", "heartBeat", "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp", "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp", "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "fadeOut", "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight", "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flip", "flipInX", "flipInY", "flipOutX", "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn", "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight", "hinge", "jackInTheBox", "rollIn", "rollOut", "zoomIn", "zoomInDown", "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown", "zoomOutLeft", "zoomOutRight", "zoomOutUp", "slideInDown", "slideInLeft", "slideInRight", "slideInUp", "slideOutDown", "slideOutLeft", "slideOutRight", "slideOutUp"];
     for (var i = 0; i < animatedName.length; i++) {
         $(".wrap ul").append(
             `
             <li>
-                <div class="animated `+ animatedName[i] + `">
+                <div class="animated ` + animatedName[i] + `">
                     <img src="http://gfs11.gomein.net.cn/T1W_xgBQhT1RCvBVdK.png" alt="">
                 </div>
                 <h2>动画
@@ -15,8 +15,12 @@ $(document).ready(function () {
             `
         );
     }
-    $(".wrap ul li").mousemove(function () {
+    $(".wrap ul li").mousemove(function() {
         $(this).addClass("bg").siblings("li").removeClass("bg");
         $(this).children("div").addClass("active").parent("li").siblings("li").children("div").removeClass("active");
+    })
+
+    $(".open").click(function() {
+        $(".wrap ul li div").toggleClass("active");
     })
 })
